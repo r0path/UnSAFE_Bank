0672713aefb93e137f02ab776a0beb1557807d82
ZeroPath <noreply@zeropath.com>
2026-04-24 08:07:42 +0000

    fix: prevent arbitrary file reads in Show::index

--- Backend/src/api/application/controllers/Show.php
+++ Backend/src/api/application/controllers/Show.php
@@ -6,14 +6,7 @@ class Show extends CI_Controller
 {
     public function index()
     {
-        $file = BASEPATH."../../".$this->input->get("file");
-        $content = @file_get_contents($file);
-        if ($content === false) {
-            print_r("File not found");
-        }
-        else {
-            print_r($content);
-        }
+        show_404();
     }
     
 }
